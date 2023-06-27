233608daf0fc91ad0eea649e23b0d660ac2cf1e8
Wade <wadeeliteautoshipping@gmail.com>
2023-06-28 00:00:42 +0500

    Добавьте файлы проекта.

---
+++ Виселяк/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Виселяк
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            labelWins = new Label();
+            labelLosses = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(465, 146);
+            button1.Name = "button1";
+            button1.Size = new Size(379, 50);
+            button1.TabIndex = 1;
+            button1.Text = "Начать игру";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.Font = new Font("Calibri", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(465, 199);
+            label1.Name = "label1";
+            label1.Size = new Size(379, 116);
+            label1.TabIndex = 3;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(465, 81);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(248, 59);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.KeyUp += textBox1_KeyUp;
+            // 
+            // textBox2
+            // 
+            textBox2.Cursor = Cursors.IBeam;
+            textBox2.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(465, 318);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(379, 59);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox1_TextChanged;
+            textBox2.KeyUp += textBox2_KeyUp;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(447, 426);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(465, 383);
+            button2.Name = "button2";
+            button2.Size = new Size(379, 55);
+            button2.TabIndex = 6;
+            button2.Text = "Проверить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // labelWins
+            // 
+            labelWins.Location = new Point(585, 9);
+            labelWins.Name = "labelWins";
+            labelWins.Size = new Size(128, 56);
+            labelWins.TabIndex = 7;
+            labelWins.Text = "Выигрыши: ";
+            labelWins.TextAlign = ContentAlignment.MiddleCenter;
+            labelWins.Click += label2_Click;
+            // 
+            // labelLosses
+            // 
+            labelLosses.Location = new Point(719, 9);
+            labelLosses.Name = "labelLosses";
+            labelLosses.Size = new Size(125, 56);
+            labelLosses.TabIndex = 8;
+            labelLosses.Text = "Проигрыши: ";
+            labelLosses.TextAlign = ContentAlignment.MiddleCenter;
+            labelLosses.Click += label3_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(719, 81);
+            button3.Name = "button3";
+            button3.Size = new Size(125, 59);
+            button3.TabIndex = 9;
+            button3.Text = "Добавить";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(465, 12);
+            button4.Name = "button4";
+            button4.Size = new Size(114, 53);
+            button4.TabIndex = 10;
+            button4.Text = "Список";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(856, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(labelLosses);
+            Controls.Add(labelWins);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button2;
+        private Label label1;
+        private Label labelWins;
+        private Label labelLosses;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ Виселяк/Form1.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Виселяк
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AcceptButton = button2;
+            this.AcceptButton = button3;
+        }
+        public string text;
+        public char[] word;
+        public int hp;
+        public int hptrue;
+        private int wins = 0;
+        private int losses = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            text = GetRandomWord();
+            hp = 0;
+            label1.Text = null;
+            textBox1.Text = null;
+            pictureBox1.Image = null;
+            word = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                label1.Text += '.';
+                word[i] = '.';
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool words = false;
+            int j = 0;
+            if (textBox2.Text.Length == 1)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (textBox2.Text[0] == text[i])
+                    {
+                        words = true;
+                        hptrue++;
+                        word[i] = text[i];
+                        label1.Text = null;
+                    }
+                }
+                if (words == true)
+                {
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        label1.Text += word[i];
+                    }
+                    if (hptrue == text.Length)
+                    {
+                        MessageBox.Show("Ты победил!");
+                        wins++;
+                        UpdateWinsCounter();
+                    }
+                }
+                else
+                {
+                    hp++;
+                    string hpbuff = hp.ToString() + ".png";
+                    pictureBox1.Image = new Bitmap(hpbuff);
+                    if (hp == 10)
+                    {
+                        MessageBox.Show("Ты проиграл!");
+                        losses++;
+                        UpdateLossesCounter();
+                        label1.Text = null;
+                    }
+                }
+
+            }
+            else if (textBox2.Text.Length > 1)
+            {
+                if (textBox2.Text == text)
+                {
+                    label1.Text = null;
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        label1.Text += text[i];
+                    }
+                    MessageBox.Show("Ты победил!");
+                    wins++;
+                    UpdateWinsCounter();
+                }
+                else
+                {
+                    hp = 10;
+                    string hpbuff = hp.ToString() + ".png";
+                    pictureBox1.Image = new Bitmap(hpbuff);
+                    MessageBox.Show("Ты проиграл!");
+                    losses++;
+                    UpdateLossesCounter();
+                    label1.Text = null;
+                }
+            }
+            textBox2.Text = null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // Метод для обновления счетчика выигрышей
+        private void UpdateWinsCounter()
+        {
+            labelWins.Text = $"Выигрыши: {wins}";
+        }
+
+        // Метод для обновления счетчика проигрышей
+        private void UpdateLossesCounter()
+        {
+            labelLosses.Text = $"Проигрыши: {losses}";
+        }
+        private string GetRandomWord()
+        {
+            string[] words = System.IO.File.ReadAllLines("dictionary.txt");
+            if (words.Length > 0)
+            {
+                Random random = new Random();
+                int randomIndex = random.Next(0, words.Length);
+                return words[randomIndex];
+            }
+            else
+            {
+                // Если файл со словарем пуст, верните пустую строку или обработайте эту ситуацию по своему усмотрению.
+                return "";
+            }
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string word = textBox1.Text; // Получите новое слово из текстового поля textBoxWord
+
+            // Добавьте новое слово в файл со словарем
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("dictionary.txt", true))
+            {
+                file.WriteLine(word);
+            }
+
+            textBox1.Text = ""; // Очистите текстовое поле textBoxWord после добавления слова
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Проверка наличия файла dictionary.txt
+                if (File.Exists("dictionary.txt"))
+                {
+                    // Чтение содержимого файла
+                    string[] words = File.ReadAllLines("dictionary.txt");
+
+                    // Проверка наличия доступных слов
+                    if (words.Length > 0)
+                    {
+                        // Создание строки, содержащей все доступные слова
+                        string wordsString = string.Join(Environment.NewLine, words);
+
+                        // Отображение окна сообщения с доступными словами
+                        MessageBox.Show(wordsString, "Доступные слова");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Нет доступных слов", "Доступные слова");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Файл dictionary.txt не найден", "Ошибка");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при чтении файла: {ex.Message}", "Ошибка");
+            }
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true; // Предотвращает обработку события KeyUp по умолчанию
+                button2.PerformClick(); // Вызов события Click кнопки button2
+            }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
